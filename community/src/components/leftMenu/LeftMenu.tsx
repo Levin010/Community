826d8e973022ff463d61f7e88b65d8b76f5d8d8f
Levin010 <levinomwenga10@gmail.com>
2025-08-08 20:49:22 +0300

    feat: add lucide-react icons to LeftMenu component and update styles for improved layout

--- community/src/components/leftMenu/LeftMenu.tsx
+++ community/src/components/leftMenu/LeftMenu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Calendar1, Home, HomeIcon, Store, User, User2Icon } from "lucide-react";
 import ProfileCard from "./ProfileCard";
 import Ad from "../Ad";
 
@@ -10,39 +11,39 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
       <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
-          <Image src="/posts.png" alt="" width={20} height={20} />
-          <span>My Posts</span>
+          <HomeIcon/>
+          <span>Home</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
-          <Image src="/activity.png" alt="" width={20} height={20} />
-          <span>Activity</span>
+          <User/>
+          <span>My Profile</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
-          <Image src="/market.png" alt="" width={20} height={20} />
+          <Store/>
           <span>Marketplace</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
-          <Image src="/events.png" alt="" width={20} height={20} />
+          <Calendar1/>
           <span>Events</span>
         </Link>
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
           <Image src="/albums.png" alt="" width={20} height={20} />
           <span>Albums</span>
@@ -50,7 +51,7 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
           <Image src="/videos.png" alt="" width={20} height={20} />
           <span>Videos</span>
@@ -58,7 +59,7 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
           <Image src="/news.png" alt="" width={20} height={20} />
           <span>News</span>
@@ -66,7 +67,7 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
           <Image src="/courses.png" alt="" width={20} height={20} />
           <span>Courses</span>
@@ -74,7 +75,7 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
           <Image src="/lists.png" alt="" width={20} height={20} />
           <span>Lists</span>
@@ -82,7 +83,7 @@ const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
         <hr className="border-t-1 border-gray-50 w-36 self-center" />
         <Link
           href="/"
-          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          className="flex items-center gap-3 p-2 rounded-lg hover:bg-slate-100"
         >
           <Image src="/settings.png" alt="" width={20} height={20} />
           <span>Settings</span>
